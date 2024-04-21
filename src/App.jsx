59ba0e30f2a5dd668aa621c5375a0a7600395923
sneharani1818 <sneharani1818@gmail.com>
2024-04-21 21:38:38 +0530

    Created new Component About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <>
     <Navbar appName="My React App"/>
-    <TextForm head="Enter your text here!!" heading='Enter your text and change it the way you want it'/>
+    {/* <TextForm head="Enter your text here!!" heading='Enter your text and change it the way you want it'/> */}
+    <About/>
     </>
   )
 }
