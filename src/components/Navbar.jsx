0dc7640c5df62d31b97f8785afe8d63c4fc12d7a
sneharani1818 <sneharani1818@gmail.com>
2024-04-22 21:00:20 +0530

    modified About.jsx file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,40 @@ import React from 'react'
 const Navbar = (props) => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg  bg-primary" >
-                <a className="navbar-brand text-white" href="#">{props.appName}</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
-                            <a className="nav-link text-white" href="/">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white" href="/">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled text-white" href="/">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" />
-                        <button className="btn btn-outline-primary my-2 my-sm-0 text-white border-white" type="submit">Search</button>
-                    </form>
+            <nav className="navbar navbar-expand-lg bg-primary text-light mb-3">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">{props.appName}</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" id='searchBar'>
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-dark light" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
             </nav>
         </div>
