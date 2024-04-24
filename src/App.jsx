1b5435f33a4ffcf50f3c6bc1721a4fe0a945c623
sneharani1818 <sneharani1818@gmail.com>
2024-04-24 19:57:03 +0530

    added dark mode feature in about.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { useState } from "react"
 import About from "./components/About"
 import Navbar from "./components/Navbar"
-import TextForm from "./components/TextForm"
+// import TextForm from "./components/TextForm"
 
 function App() {
-
+  const [darkmode, setdarkmode]=useState(false);
   return (
     <>
     <Navbar appName="My React App"/>
-    {/* <TextForm head="Enter your text here!!" heading='Enter your text and change it the way you want it'/> */}
-    <About/>
+    <TextForm head="Enter your text here!!" heading='Enter your text and change it the way you want it'/>
+    {/* <About/> */}
     </>
   )
 }
